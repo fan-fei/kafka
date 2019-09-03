1b6a157e545efd6dc3e4e643a74b76739d2c9365
樊飞 <fanfei@ctv.org.cn>
2019-09-03 17:20:29 +0800

    update

--- producer/src/main/java/com/nordsoft/producer/ProducerApplication.java
+++ producer/src/main/java/com/nordsoft/producer/ProducerApplication.java
@@ -2,7 +2,6 @@ package com.nordsoft.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
