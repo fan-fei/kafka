347f22e74184c85c91c08734dd6176c4e0480a1f
樊飞 <fanfei@ctv.org.cn>
2019-09-04 11:11:23 +0800

    update

--- consumer/src/main/java/com/nordsoft/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/nordsoft/consumer/ConsumerApplication.java
@@ -1,10 +1,11 @@
 package com.nordsoft.consumer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
@@ -12,13 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ConsumerApplication {
 
+    @Autowired
+    private Registration registration;
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
 
-    @GetMapping(value = "/consumer/hello/{code}")
-    public String get(@PathVariable("code") String code) {
-        return "HELLO!";
+    @RequestMapping(value = "/consumer/hello")
+    public String get() {
+        return registration.getInstanceId();
     }
 }
--- producer/src/main/java/com/nordsoft/producer/ProducerApplication.java
+++ producer/src/main/java/com/nordsoft/producer/ProducerApplication.java
@@ -1,9 +1,10 @@
 package com.nordsoft.producer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProducerApplication {
 
+    @Autowired
+    private Registration registration;
 
     public static void main(String[] args) {
         SpringApplication.run(ProducerApplication.class, args);
     }
 
-    @RequestMapping(value = "/producer/hello/{code}")
-    public String get(@PathVariable("code") String code) {
-        return "HELLO!";
+    @RequestMapping(value = "/producer/hello")
+    public String get() {
+        return registration.getInstanceId();
     }
+
 }
 
