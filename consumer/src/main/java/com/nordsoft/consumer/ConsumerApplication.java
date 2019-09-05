ee1ed9d30421096d8d290164ac3900f2a67961ac
樊飞 <fanfei@ctv.org.cn>
2019-09-05 18:41:39 +0800

    update

--- consumer/src/main/java/com/nordsoft/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/nordsoft/consumer/ConsumerApplication.java
@@ -1,27 +1,53 @@
 package com.nordsoft.consumer;
 
+import com.google.common.collect.Maps;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.messaging.Message;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 @EnableDiscoveryClient
 @SpringBootApplication
 @RestController
 public class ConsumerApplication {
 
     @Autowired
+    private StringRedisTemplate template;
+    @Autowired
     private Registration registration;
+
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
 
     @GetMapping(value = "/consumer/hello")
-    public String get() {
-        return registration.getInstanceId();
+    public Map get() {
+        Map<String, String> dataMap = Maps.newHashMap();
+        Set<String> keys = template.keys("com.nordsoft.streams.*");
+        for (String key : keys) {
+            dataMap.put(key, template.opsForValue().get(key));
+        }
+        return dataMap;
+    }
+
+    @StreamListener
+    public void receive(Message<String> message) {
+        String instanceId = registration.getInstanceId();
+        String msg = message.getPayload();
+        template.opsForValue().set("com.nordsoft.streams." + instanceId, msg, 10, TimeUnit.SECONDS);
+        log.info("consumer:{},message:{}", instanceId, msg);
     }
 }
--- producer/src/main/java/com/nordsoft/producer/ProducerApplication.java
+++ producer/src/main/java/com/nordsoft/producer/ProducerApplication.java
@@ -1,13 +1,19 @@
 package com.nordsoft.producer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
+@Slf4j
 @EnableDiscoveryClient
 @SpringBootApplication
 @RestController
@@ -16,13 +22,18 @@ public class ProducerApplication {
     @Autowired
     private Registration registration;
 
+    @Resource
+    private MessageChannel messageChannel;
+
     public static void main(String[] args) {
         SpringApplication.run(ProducerApplication.class, args);
     }
 
     @GetMapping(value = "/producer/hello")
-    public String get() {
-        return registration.getInstanceId();
+    public Boolean get() {
+        String instanceId = registration.getInstanceId();
+        log.info("producer:{},message:{}", instanceId, instanceId);
+        return messageChannel.send(MessageBuilder.withPayload(instanceId).build());
     }
 
 }
