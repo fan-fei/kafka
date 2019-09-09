49b68b87913ca5e4955d1439179daa9ffbf5d3fd
樊飞 <fanfei@ctv.org.cn>
2019-09-09 17:53:42 +0800

    update

--- consumer/src/main/java/com/nordsoft/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/nordsoft/consumer/ConsumerApplication.java
@@ -1,5 +1,6 @@
 package com.nordsoft.consumer;
 
+import cn.hutool.core.util.IdUtil;
 import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +12,12 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.messaging.Message;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -48,12 +51,13 @@ public class ConsumerApplication {
     }
 
     @StreamListener(Sink.INPUT)
-    public void receive(Object message) {
+    public void receive(Message<?> message) {
         String instanceId = registration.getInstanceId();
-        String msg = message.toString();
-        template.opsForHash().put(COM_NORDSOFT_STREAMS + instanceId, "producer", msg);
-        template.opsForHash().put(COM_NORDSOFT_STREAMS + instanceId, "timestamp", LocalDateTime.now().toString());
-        template.expire(COM_NORDSOFT_STREAMS + instanceId, 30, TimeUnit.MINUTES);
+        String msg = message.getPayload().toString();
+        String key = COM_NORDSOFT_STREAMS + instanceId + "." + IdUtil.createSnowflake(1, 1).nextId();
+        template.opsForHash().put(key, "producer", msg);
+        template.opsForHash().put(key, "timestamp", LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        template.expire(key, 30, TimeUnit.MINUTES);
         log.info("consumer:{},message:{}", instanceId, msg);
     }
 }
